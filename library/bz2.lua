0c950650ee8930dbe331b9f987300efb751fbd68
Maticzpl <czerneckimateusz0@gmail.com>
2024-03-28 23:59:18 +0100

    Update bz2 declarations
    Resolves #14

--- library/bz2.lua
+++ library/bz2.lua
@@ -9,55 +9,90 @@
 -- Unless stated otherwise, all functions raise errors if supplied with parameters that disagree with their descriptions. 
 bz2 = {}
 
+-- TODO Removed in v98?? idk, check later
 ---@enum bz2Result
-bz2 = {
-    --Compression OK
-    compressOk = 0,
+-- bz2 = {
+--     --Compression OK
+--     compressOk = 0,
+--
+--     --Compression failed, ran out of memory
+--     compressNomem = 1,
+--
+--     --Compression failed, maxSize limit exceeded
+--     compressLimit = 2,
+--
+--     --Decompression OK
+--     decompressOk = 0,
+--
+--     --Decompression failed, ran out of memory
+--     decompressNomem = 4,
+--
+--     --Decompression failed, maxSize limit exceeded
+--     decompressLimit = 2,
+--
+--     --Decompression failed, sourceData does not have bzip2 header and is likely not bzip2 data
+--     decompressType = 3,
+--
+--     --Decompression failed, sourceData is not valid bzip2 data
+--     decompressBad = 4,
+--     
+--     decompressEof = 5,
+-- }
 
-    --Compression failed, ran out of memory
-    compressNomem = 1,
+---@alias bz2CompressErr
+---|`bz2.COMPRESS_NOMEM`
+---|`bz2.COMPRESS_LIMIT`
+---|nil
 
-    --Compression failed, maxSize limit exceeded
-    compressLimit = 2,
-
-    --Decompression OK
-    decompressOk = 0,
-
-    --Decompression failed, ran out of memory
-    decompressNomem = 4,
-
-    --Decompression failed, maxSize limit exceeded
-    decompressLimit = 2,
-
-    --Decompression failed, sourceData does not have bzip2 header and is likely not bzip2 data
-    decompressType = 3,
-
-    --Decompression failed, sourceData is not valid bzip2 data
-    decompressBad = 4,
-    
-    decompressEof = 5,
-}
+---@alias bz2DecompressErr
+---|`bz2.DECOMPRESS_NOMEM`
+---|`bz2.DECOMPRESS_LIMIT`
+---|`bz2.DECOMPRESS_TYPE`
+---|`bz2.DECOMPRESS_BAD`
+---|`bz2.DECOMPRESS_EOF`
+---|nil
 
 --```
---bz2.compress(sourceData, maxSize)
+--compressedData, errCode, errStr = bz2.compress(data, [maxSize])
 --```
--- Accepts source data and returns a string containing the compressed data. maxSize controls the maximum amount of memory to use while compressing the sourceData, and can be 0 to allow unlimited memory.<br>
--- On success, returns 1 argument - the compressed data<br>
--- On failure, returns 3 arguments - nil, error code, error string. Error code will be one of the bz2. constants
+--Compress data with bzip2 at compression level 9.<br>
+--  - `data`: string, the data to be compressed<br>
+--  - `maxSize`: number, upper limit on the length of `compressedData`; defaults to `0`, which means no limit<br>
+--  - `compressedData`: string, the compressed data, or `nil` on error<br>
+--  - `errCode`: `nil`, or one of the following values on error:<br>
+--      - `bz2.COMPRESS_NOMEM`: out of memory, TPT will probably crash soon<br>
+--      - `bz2.COMPRESS_LIMIT`: the length of `compressedData` would exceed `maxSize`<br>
+--  - `errStr`: `nil`, or a human-friendly string that explains the error<br>
 ---@param sourceData string
 ---@param maxSize integer?
----@return string | nil, bz2Result, string
+---@return string | nil, bz2CompressErr, string | nil
 function bz2.compress(sourceData, maxSize) end
 
 --```
---bz2.decompress(sourceData, maxSize)
+--data, errCode, errStr = bz2.decompress(compressedData, [maxSize])
 --```
--- Accepts source data and returns a string containing the decompressed data. maxSize controls the maximum amount of memory to use while decompressing the sourceData, and can be 0 to allow unlimited memory.
--- On success, returns 1 argument - the decompressed data
--- On failure, returns 3 arguments - nil, error code, error string. Error code will be one of the bz2. constants
+--Decompress bzip2-compressed data.<br>
+--  - `compressedData`: string, the compressed data<br>
+--  - `maxSize`: number, upper limit on the length of data; defaults to 0, which means no limit<br>
+--  - `data`: string, the original data, or nil on error<br>
+--  - `errCode`: nil, or one of the following values on error:<br>
+--      - `bz2.DECOMPRESS_NOMEM`: out of memory, TPT will probably crash soon<br>
+--      - `bz2.DECOMPRESS_LIMIT`: the length of data would exceed maxSize<br>
+--      - `bz2.DECOMPRESS_TYPE`: compressedData is not bzip2-compressed data<br>
+--      - `bz2.DECOMPRESS_BAD`: compressedData is otherwise corrupt<br>
+--      - `bz2.DECOMPRESS_EOF`: compressedData contains data beyond the end of the bzip2 stream<br>
+--  - `errStr`: nil, or a human-friendly string that explains the error<br>
 ---@param sourceData string
 ---@param maxSize integer?
----@return string | nil, bz2Result, string
+---@return string | nil, bz2DecompressErr, string | nil
 function bz2.decompress(sourceData, maxSize) end
 
 
+bz2.COMPRESS_NOMEM = 1
+bz2.COMPRESS_LIMIT = 2
+
+bz2.DECOMPRESS_NOMEM = 1
+bz2.DECOMPRESS_LIMIT = 2
+bz2.DECOMPRESS_TYPE = 3
+bz2.DECOMPRESS_BAD = 4
+bz2.DECOMPRESS_EOF = 5
